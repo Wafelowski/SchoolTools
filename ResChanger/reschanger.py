67d52657d8a41831c576fc0060d0a03f83d33e5c
Wafel <heavywolfpl@gmail.com>
2023-05-10 12:12:15 +0200

    ResChanger 1.5

--- ResChanger/reschanger.py
+++ ResChanger/reschanger.py
@@ -2,19 +2,60 @@ import os
 import time
 from PIL import Image
 
-original_path = "./original/"
-modified_path = "./modified/"
-if os.path.exists(original_path) == False:
-    os.mkdir(original_path)
-if os.path.exists(modified_path) == False:
-    os.mkdir(modified_path)
+if not os.path.exists("modified"):
+    os.makedirs("modified")
 
-original = os.listdir(original_path) # get the list of files in folder with unchanged (original) images
-print(f"Lista Plików ({len(original)}): \n{original}\n")
+directory = os.getcwd()
+folders = []
+def listdirs(folder):
+    for file in os.listdir(folder):
+        d = os.path.join(folder, file)
+        if os.path.isdir(d):
+            folders.append(d)
+            listdirs(d)
 
-temp = input("Czy lista plików się zgadza? (t/n): ")
+folder = os.getcwd()
+listdirs(os.getcwd())
+
+files = []
+for folder in folders:
+    current = []
+    for file in os.listdir(folder):
+        d = os.path.join(folder, file)
+        if os.path.isfile(d):
+            current.append(d)
+
+    if current != []:
+        files.append(current)
+
+
+def count_items(nested_list):
+    count = 0
+    for item in nested_list:
+        if isinstance(item, list):
+            count += count_items(item)
+        else:
+            count += 1
+    return count
+
+def flatten_list(nested_list):
+    flat_list = []
+    for item in nested_list:
+        if isinstance(item, list):
+            flat_list.extend(flatten_list(item))
+        else:
+            flat_list.append(item)
+    return flat_list
+
+count = count_items(files)
+files = flatten_list(files)
+print(f"Lista Plików ({count}):")
+for file in files:
+    print(file)
+
+temp = input(f"Czy lista plików {count} się zgadza? (t/n): ")
 if temp.lower() != "t":
-    print("\nPamietaj ze program wyszukuje pliki w folderze original, ktory znajduje sie w tym samym folderze co plik wykonywalny!")
+    print("\nPamietaj ze program wyszukuje pliki w folderze z plikiem wykonywalnym!")
     print("Anuluje...")
     time.sleep(5)
     exit()
@@ -29,10 +70,10 @@ if int(size) < 1:
     exit()
 
 
-for x in original:
-    if (x.lower().endswith(".jpeg") or x.lower().endswith(".jpg") or x.lower().endswith(".png")):
-        print(f"Obraz - {x}")
-        img = Image.open(f"{original_path}/{x}")
+for file in files:
+    if (file.lower().endswith(".jpeg") or file.lower().endswith(".jpg") or file.lower().endswith(".png")):
+        print(f"Obraz - {file}")
+        img = Image.open(f"{file}")
         width, height = img.size
         print(f"Stare rozmiary - {img.size}")
         if width >= height:
@@ -41,21 +82,22 @@ for x in original:
             longer = "height"
         if longer == "width":
             xx = width / size
-            # print(xx)
             height = height / xx
             width = width / xx
-            # print(height)
-            # print(width)
         elif longer == "height":
             xx = height / size
-            # print(xx)
             height = height / xx
             width = width / xx
-            # print(height)
-            # print(width)
+        
+        dir_path, file_name = os.path.split(file)
+        dir_path = dir_path.replace(f"{directory}", f"{directory}\\modified")
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+
+        file_name = file.replace(f"{directory}", f"{directory}\\modified")
         img = img.resize((round(width), round(height)), Image.ANTIALIAS)
-        img.save(f"{modified_path}/{x}")
-        img2 = Image.open(f"{modified_path}/{x}")
+        img.save(f"{file_name}")
+        img2 = Image.open(f"{file_name}")
         print(f"Dluzsza strona - {longer}")
         print(f"Nowe rozmiary: {img2.size}")
         print("------------------")
